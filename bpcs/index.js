3819e7c5fe27c781028f07bbb3a43b42ce282afa
Jauhar Arifin <jauhararifin10@gmail.com>
2018-02-13 13:13:47 +0700

    read image and serve image

--- bpcs/index.js
+++ bpcs/index.js
@@ -1,11 +1,13 @@
+var jimp = require('jimp')
+
 module.exports = function (req, res, next) {
-    let stegoImage = req.files['image']
+    let stegoImage = req.files['image'][0]
     if (!stegoImage) {
         res.status(500).send('Must provide image file')
         return
     }
     
-    let plainFile = req.files['plainFile']
+    let plainFile = req.files['plainFile'][0]
     if (!plainFile) {
         res.status(500).send('Must specify file to insert')
         return
@@ -17,11 +19,21 @@ module.exports = function (req, res, next) {
         return
     }
 
-    let outputType = req.body.outputType
-    if (!outputType)
-        outputType = 'image/bmp'
-    if (!['image/bmp', 'image/png'].includes(outputType))
-        outputType = 'image/bmp'
-
-    res.send('inserting image')
+    let outputMimeType = req.body.outputType
+    if (!outputMimeType)
+        outputMimeType = 'image/bmp'
+    if (outputMimeType == 'image/png')
+        outputMimeType = jimp.MIME_PNG
+    else
+        outputMimeType = jimp.MIME_BMP
+        
+    jimp.read(stegoImage.path).then(function(img) {
+        // do stegano
+        return img
+    }).then(function(img) {
+        img.getBuffer(outputMimeType, function(err, buffer) {
+            res.set("Content-Type", outputMimeType);
+            res.send(buffer);
+        })
+    })
 }
