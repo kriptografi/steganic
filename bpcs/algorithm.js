31fcf0997446df1509913cfebb7fb76f75503a42
Jauhar Arifin <jauhararifin10@gmail.com>
2018-02-13 15:10:56 +0700

    add conjugate function

--- bpcs/algorithm.js
+++ bpcs/algorithm.js
@@ -7,6 +7,22 @@ function unsetBit(bit, i) {
     return bit &= ~(1 << i)
 }
 
+function conjugate(bitplane) {
+    let height = bitplane.length
+    if (height <= 0)
+        return 0
+    let width = bitplane[0].length
+
+    let result = []
+    for (let i = 0; i < height; i++) {
+        result[i] = []
+        for (let j = 0; j < width; j++)
+            result[i].push(bitplane[i][j] ^ (i % 2) ^ (j % 2))
+    }
+
+    return result
+}
+
 function tooglePbcCgc(bitplane) {
     let height = bitplane.length
     if (height <= 0)
@@ -17,7 +33,7 @@ function tooglePbcCgc(bitplane) {
     for (let i = 0; i < height; i++) {
         result[i] = [bitplane[i][0]]
         for (let j = 1; j < width; j++)
-            result[i][j].push(bitplane[i][j] ^ bitplane[i][j-1])
+            result[i].push(bitplane[i][j] ^ bitplane[i][j-1])
     }
 
     return result
