fa3a21f900433f4e4faf222ace7f3a7bacc9e1b7
Jauhar Arifin <jauhararifin10@gmail.com>
2018-02-13 15:25:30 +0700

    randomized image

--- bpcs/algorithm.js
+++ bpcs/algorithm.js
@@ -75,7 +75,7 @@ module.exports = function(spec) {
 
     for (let i = 0; i < height; i++)
         for (let j = 0; j < width; j++)
-            image.setPixelColor(0x000000, j, i)
+            image.setPixelColor(Math.floor(Math.random()*0xffffffff), j, i)
 
     return image
 }
