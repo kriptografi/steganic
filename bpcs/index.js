49d6d90ef9c88e61b5f688b91415a9663b707e6b
Jauhar Arifin <jauhararifin10@gmail.com>
2018-02-13 12:12:53 +0700

    add logic for getting files and keys

--- bpcs/index.js
+++ bpcs/index.js
@@ -1,5 +1,27 @@
 module.exports = function (req, res, next) {
-    console.log(req.files['image'])
-    console.log(req.files['plainFile'])
-    console.log(req.body)
+    let stegoImage = req.files['image']
+    if (!stegoImage) {
+        res.status(500).send('Must provide image file')
+        return
+    }
+    
+    let plainFile = req.files['plainFile']
+    if (!plainFile) {
+        res.status(500).send('Must specify file to insert')
+        return
+    }
+
+    let key = req.body.key
+    if (!key) {
+        res.status(500).send('Must specify key')
+        return
+    }
+
+    let outputType = req.body.outputType
+    if (!outputType)
+        outputType = 'image/bmp'
+    if (!['image/bmp', 'image/png'].includes(outputType))
+        outputType = 'image/bmp'
+
+    res.send('inserting image')
 }
