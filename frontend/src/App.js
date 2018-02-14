64090cf5c18d4a1f353caf9f0f2b6d7fa54b9f43
Jauhar Arifin <jauhararifin10@gmail.com>
2018-02-14 11:45:59 +0700

    add frontend using react

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Jauhar Arifin
+      </div>
+    );
+  }
+}
+
+export default App;
