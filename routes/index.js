2a10beb7a029c203de6aa3bc4bea3ba8e8113e39
Jauhar Arifin <jauhararifin10@gmail.com>
2018-02-15 16:41:46 +0700

    add routing to retrieve feature

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = require('../bpcs/route')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.post('/stego', require('../bpcs/route'))
 
 module.exports = router;
